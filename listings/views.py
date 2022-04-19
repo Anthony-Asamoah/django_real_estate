c5a03e1123c0903bdda2de086e0adba7682f77c6
Anthony-Asamoah <anthonyasamoah48@gmail.com>
2022-04-19 23:21:48 +0000

    wrote queries for front end; listings and realtors

--- listings/views.py
+++ listings/views.py
@@ -1,14 +1,33 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from .models import Listing
 
 # Create your views here.
 
+
 def index(request):
-	return render(request, 'listings/listings.html', {'page_title': 'Listings'})
+	query = Listing.objects.all().order_by('-listing_date').filter(is_published=True)
+
+	paginator = Paginator(query, 6)
+	page = request.GET.get('page')
+
+	paged_listing = paginator.get_page(page)
+
+	context = {
+		'page_title': 'Listings',
+		'query': paged_listing
+	}
+	return render(request, 'listings/listings.html', context)
 
 
-def listing(request):
-	return render(request, 'listings/listing.html', {'page_title': 'Listing'})
+def listing(request, listing_id):
+	query = get_object_or_404(Listing, pk=listing_id)
+
+	context = {
+		'page_title': 'Listing',
+		'listing': query
+	}
+	return render(request, 'listings/listing.html', context)
 
 
 def search(request):
--- pages/views.py
+++ pages/views.py
@@ -1,19 +1,29 @@
 from django.shortcuts import render, HttpResponse
+from listings.models import Listing
+from realtors.models import realtor
 
 # Create your views here.
 
 
 def index(request):
+	query = Listing.objects.all().order_by('-listing_date').filter(is_published=True)[:3]
+
 	context = {
 		'page_title': 'Home',
+		'listing': query
 	}
 
 	return render(request, 'pages/index.html', context)
 
 
 def about(request):
+	query = realtor.objects.order_by('hire_date')
+	mvp = query.filter(is_mvp=True)
+
 	context = {
 		'page_title': 'About',
+		'realtor': query,
+		'mvp': mvp
 	}
 
 	return render(request, 'pages/about.html', context)
