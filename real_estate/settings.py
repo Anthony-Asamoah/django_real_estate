c770aad4119cd668d84658488cd524ac69148828
QuanChii <anthonyasamoah48@gmail.com>
2026-05-20 21:50:10 +0000

    move sensitive settings to .env

--- real_estate/settings.py
+++ real_estate/settings.py
@@ -13,6 +13,7 @@ import os.path
 from pathlib import Path
 from os import path
 from django.contrib.messages import constants as messages
+from decouple import config, Csv
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,15 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_iglq=-m9w1(*ied5_*h8#1r)ldd=euqcw8(f#zl$d%7*x+h46'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = [
-	'localhost',
-	'127.0.0.1',
-]
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Application definition
 
@@ -88,11 +86,11 @@ WSGI_APPLICATION = 'real_estate.wsgi.application'
 DATABASES = {
 	'default': {
 		'ENGINE': 'django.db.backends.postgresql_psycopg2',
-		'NAME': 'django_real_estate_btre',
-		'USER': 'postgres',
-		'PASSWORD': 'admin1234',
-		'HOST': 'localhost',
-		'PORT': 5000
+		'NAME': config('DB_NAME'),
+		'USER': config('DB_USER'),
+		'PASSWORD': config('DB_PASSWORD'),
+		'HOST': config('DB_HOST', default='localhost'),
+		'PORT': config('DB_PORT', default=5432, cast=int),
 	}
 }
 
@@ -154,7 +152,7 @@ MESSAGE_TAGS = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'anthonyasamoah48@gmail.com'
-EMAIL_HOST_PASSWORD = 'fdjwsuscxzbhrpnc'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 
