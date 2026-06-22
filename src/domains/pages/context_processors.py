56502069fc8dff43cd3031049f721451d780266a
QuanChii <anthonyasamoah48@gmail.com>
2026-06-22 11:54:41 +0000

    add vercel analytics

--- src/domains/pages/context_processors.py
+++ src/domains/pages/context_processors.py
@@ -5,4 +5,5 @@ def site_name(request):
     return {
         'SITE_NAME': getattr(settings, 'SITE_NAME', 'Real Estate'),
         'RECAPTCHA_SITE_KEY': getattr(settings, 'RECAPTCHA_SITE_KEY', ''),
+        'DEBUG': settings.DEBUG,
     }
