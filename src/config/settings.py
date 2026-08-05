e2a6952d2b8890097ca43f7f202ec56e57b3c7b6
QuanChii <anthonyasamoah48@gmail.com>
2026-08-05 12:50:01 +0000

    configure to use mailpit for email

--- src/config/settings.py
+++ src/config/settings.py
@@ -229,9 +229,9 @@ DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='noreply@example.com')
 
 # SMTP settings (used when EMAIL_PROVIDER=django_smtp)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_HOST = config('EMAIL_HOST', default='localhost')
+EMAIL_PORT = config('EMAIL_PORT', default=1025, cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=False, cast=bool)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 
